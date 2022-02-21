32b35e58244a896707d462ab032aba9d705a649f
saroeunsaing <guru.tech.kh@gmail.com>
2022-02-21 13:38:19 +0700

    First Commit

--- Library Management System/Form1.Designer.vb
+++ Library Management System/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(120, 0)
+        Me.ClientSize = New System.Drawing.Size(214, 128)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
