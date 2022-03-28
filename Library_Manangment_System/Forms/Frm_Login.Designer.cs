31aedd043c22ca30f4a3ba57272f932970252b1c
GURUTECH <GURUTECH@GURUTECH>
2022-03-28 14:10:40 +0700

    Login From

---
+++ Library_Manangment_System/Forms/Frm_Login.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Library_Manangment_System.Forms
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 170);
+            this.Name = "Frm_Login";
+            this.Text = "Frm_Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
